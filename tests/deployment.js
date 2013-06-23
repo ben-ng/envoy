a52f340ff5fece320182b233b7dc9cdaf2390e59
Ben Ng <me@benng.me>
2013-06-22 19:02:55 -0700

    Add s3 adapter

--- tests/deployment.js
+++ tests/deployment.js
@@ -4,21 +4,32 @@ var assert = require('assert')
   , envoy = require('../lib/envoy.js')
   , secrets = require(process.env.SECRETS_FILE)
   , fixtures = require('./fixtures')
-  , adapters = ["memory", "ftp"]
+  , adapters = []
   , undeployOpts
+  //Add to this array new adapters to test
   , opts = {
-      memory: {
-        baseDir:'./tests'
-      }
+      memory: {}
+    , s3: secrets.s3
     , ftp: secrets.ftp
-    }
-  , undeploy = function () {
-      
     };
 
-_.extend(opts.ftp, {baseDir:'./tests'});
+_.each(opts, function (opts, adapter) {
+  adapters.push(adapter.toLowerCase());
+});
+
+//Load s3 configuration from travis if possible
+if(process.env.TRAVIS_SECURE_ENV_VARS) {
+  _.extend(opts.s3,{
+    key: process.env.AWS_S3_KEY
+  , secret: process.env.AWS_S3_SECRET
+  , bucket: process.env.AWS_S3_BUCKET
+  , region: process.env.AWS_S3_REGION
+  });
+}
 
 _.each(adapters, function (adapterName) {
+
+  _.extend(opts[adapterName], {baseDir:'./tests'});
   
   tests[adapterName + ' deploy/undeploy empty collection'] = function (next) {
     envoy.deployCollection({}, adapterName, opts[adapterName], function (err, log, hashes) {
